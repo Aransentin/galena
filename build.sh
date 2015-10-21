a8230e4ea6ee9a474f61db2d9789d709c9b5efb8
Jens Goldberg <jens.goldberg@gmail.com>
2015-10-21 11:25:36 +0200

    added build arguments

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+#possible arguments: (e.g. run './build.sh DEBUG NOSHARING' )
+#	DEBUG		Print performance measurements, analyze return codes and such
+#	NOSHARING	Don't use the CL_KHR_gl_sharing extension - slower, but wider support
+
+for var in "$@"; do
+	DEFINES="$DEFINES -D$var"
+done
+
+OPT="-Ofast"
+WARN="-Weverything -Wno-padded -Wno-format-nonliteral"
+LIBS="-lm -lglfw -lOpenCL"
+
 cat kernels/*.cl | ./bufferize.py > src/kernels.src.h
-clang -std=c11 -Weverything -Wno-padded -Wno-format-nonliteral -DDEBUG -Ofast -flto src/*.c -lm -lglfw -lOpenCL -o bin.elf
+clang -std=c11 $WARN $DEFINES $OPT src/*.c $LIBS -o bin.elf
